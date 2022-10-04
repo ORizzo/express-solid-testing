0b5d45d92b908d3f4836e17d63a1a4053f3fa3d7
ORizzo <henriquerizzomor@gmail.com>
2022-10-04 20:40:56 -0300

    Adjust on GetBookController

--- src/controllers/getBookController.ts
+++ src/controllers/getBookController.ts
@@ -11,9 +11,10 @@ class GetBookController {
       if (bookToSearch) {
         const book = await service.execute(bookToSearch);
         return res.json(book);
+      } else {
+        const book = await service.execute();
+        return res.json(book);
       }
-      const book = await service.execute();
-      return res.json(book);
     } catch (error) {
       const { message, statusCode } = error as APIError;
       res.status(statusCode).json({ message: message });
