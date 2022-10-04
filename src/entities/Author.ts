075fc20012876ceb41b7016c72fcb7feaca7f683
ORizzo <henriquerizzomor@gmail.com>
2022-10-03 21:48:58 -0300

    Add the Author and book entities and their folder

---
+++ src/entities/Author.ts
@@ -0,0 +1,5 @@
+export interface Author {
+  id: Number;
+  createdAt: Date;
+  name: String;
+}
---
+++ src/entities/Book.ts
@@ -0,0 +1,6 @@
+export interface Book {
+  id: number;
+  createdAt: Date;
+  title: string;
+  author_Id: number | null;
+}
