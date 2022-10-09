b8b5e9094d77a113ad960f09c3442e9828b02f0e
ORizzo <henriquerizzomor@gmail.com>
2022-10-08 23:04:08 -0300

    Add delete book service

---
+++ src/services/deleteBookService.ts
@@ -0,0 +1,21 @@
+import { Book } from "../entities/Book";
+import { BookRepository, BookToDelete } from "../repository/bookRepository";
+import { APIError } from "../utils/APIError";
+
+class DeleteBookService {
+  async execute(bookToDelete: BookToDelete): Promise<Book | APIError> {
+    const { bookName } = bookToDelete;
+    const bookExists = await BookRepository.get(bookName);
+    if (!bookExists) {
+      throw new APIError(
+        `The book which you want to delete do not exists in the database, book: ${bookName}`,
+        409
+      );
+    } else {
+      const deletedBook = await BookRepository.delete(bookToDelete);
+      return deletedBook;
+    }
+  }
+}
+
+export { DeleteBookService };
