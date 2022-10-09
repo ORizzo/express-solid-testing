8d601e40087be9698caa22ab0019fea97c3df4c2
ORizzo <henriquerizzomor@gmail.com>
2022-10-08 23:03:23 -0300

    Fix integration test, Add before and after all, and Add unit test for delelte, post and get service

---
+++ src/tests/app.spec.ts
@@ -0,0 +1,57 @@
+import app from "../app";
+import { BookRepository } from "../repository/bookRepository";
+import { test, expect, describe, beforeAll } from "vitest";
+import { agent } from "supertest";
+
+const mock = {
+  author: "Henrique Rizzo",
+  bookName: "Cálice de 200",
+  newBookName: "Cálice de 204",
+};
+
+beforeAll(async () => {
+  const bookExits = await BookRepository.get(mock.bookName);
+  if (bookExits) {
+    await BookRepository.delete({
+      author: mock.author,
+      bookName: mock.bookName,
+    });
+  }
+});
+
+describe("Integration test", async () => {
+  test("Should create a book in the database", async () => {
+    const response = await agent(app).post("/book").send({
+      author: mock.author,
+      bookName: mock.bookName,
+    });
+    expect(response.body).toBeTruthy();
+    expect(response.statusCode).toEqual(201);
+  });
+  test("Should get a book from the database", async () => {
+    const response = await agent(app).get("/book");
+    const books = response.body;
+    const book = books.find((element) => element.name == mock.bookName);
+    expect(book.name).toEqual(mock.bookName);
+    expect(response.statusCode).toEqual(200);
+  });
+  test("Should update a book from the database", async () => {
+    const response = await agent(app).put("/book").send({
+      bookName: mock.bookName,
+      newBookName: mock.newBookName,
+    });
+    const updatedBook = response.body;
+
+    expect(updatedBook.name).toEqual(mock.newBookName);
+    expect(response.statusCode).toEqual(200);
+  });
+  test("Should delete a book from the database", async () => {
+    const response = await agent(app).delete("/book").send({
+      bookName: mock.newBookName,
+    });
+    const deletedBook = response.body;
+
+    expect(deletedBook.name).toEqual(mock.newBookName);
+    expect(response.statusCode).toEqual(200);
+  });
+});
---
+++ src/tests/deleteBookService.spec.ts
@@ -0,0 +1,50 @@
+import { test, expect, describe, beforeAll, afterAll } from "vitest";
+import { DeleteBookService } from "../services/deleteBookService";
+import { BookRepository } from "../repository/bookRepository";
+import { APIError } from "../utils/APIError";
+
+const mock = {
+  author: "Mock Author",
+  bookName: "Mock Book",
+};
+
+beforeAll(async () => {
+  const result = await BookRepository.get(mock.bookName);
+  if (!result) {
+    await BookRepository.create({
+      author: mock.author,
+      bookName: mock.bookName,
+    });
+  }
+});
+
+describe("Delete book service unit tests", async () => {
+  test("Delete book service if have an argument should return a new created book", async () => {
+    const service = new DeleteBookService();
+    const result = await service.execute(mock);
+    expect(result).toHaveProperty("id");
+  });
+  test("Delete book service if try to delete a nonexistent book, it will fail", async () => {
+    try {
+      const service = new DeleteBookService();
+      await service.execute(mock);
+    } catch (error) {
+      const { message, statusCode } = error as APIError;
+      expect(error).toBeInstanceOf(APIError);
+      expect(message).toEqual(
+        `The book which you want to delete do not exists in the database, book: ${mock.bookName}`
+      );
+      expect(statusCode).toEqual(409);
+    }
+  });
+});
+
+afterAll(async () => {
+  const result = await BookRepository.get(mock.bookName);
+  if (result) {
+    await BookRepository.delete({
+      author: mock.author,
+      bookName: mock.bookName,
+    });
+  }
+});
