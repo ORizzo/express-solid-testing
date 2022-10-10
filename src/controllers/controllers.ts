a64a6710dfe7f565f7e58210f5653cc80d029e79
ORizzo <henriquerizzomor@gmail.com>
2022-10-09 21:09:15 -0300

    Add an exporter file to controllers

---
+++ src/controllers/controllers.ts
@@ -0,0 +1,5 @@
+export { GetAuthorController } from "./Author/getAuthorController";
+export { GetBookController } from "./Book/getBookController";
+export { PostBookController } from "./Book/postBookController";
+export { PutBookController } from "./Book/putBookController";
+export { DeleteBookController } from "./Book/deleteBookController";
