d39199de6ded916d3ea5794249ef755a4266b8d4
ORizzo <henriquerizzomor@gmail.com>
2022-10-09 21:05:56 -0300

    Await on put book service call

--- src/controllers/Book/putBookController.ts
+++ src/controllers/Book/putBookController.ts
@@ -6,8 +6,8 @@ class PutBookController {
   static async handle(req: Request, res: Response) {
     try {
       const service = new PutBookService();
-      const updatedBook = service.execute(req.body);
-      res.status(201).json({ message: updatedBook });
+      const updatedBook = await service.execute(req.body);
+      res.status(201).json(updatedBook);
     } catch (error) {
       const { statusCode, message } = error as APIError;
       res.status(statusCode).json({ message: message });
